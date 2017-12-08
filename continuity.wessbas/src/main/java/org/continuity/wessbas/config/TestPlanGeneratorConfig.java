1d59bd1b43e895258eee0f8b5fa55c3690269bcd
Henning Schulz <henning.schulz@novatec-gmbh.de>
2017-12-08 15:50:16 +0100

    CONT-4: Use sensible Logging.

--- continuity.wessbas/src/main/java/org/continuity/wessbas/config/TestPlanGeneratorConfig.java
+++ continuity.wessbas/src/main/java/org/continuity/wessbas/config/TestPlanGeneratorConfig.java
@@ -1,6 +1,8 @@
 package org.continuity.wessbas.config;
 
 import org.continuity.wessbas.transform.jmeter.WessbasToJmeterConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +14,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TestPlanGeneratorConfig {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(TestPlanGeneratorConfig.class);
+
 	@Bean
 	WessbasToJmeterConverter wessbasToJmeterConverter(@Value("${testplan.configuration.path:configuration}") String configurationPath) {
-		System.out.println("###\n###   Configuration Path is " + configurationPath + "\n###");
+		LOGGER.info("Testplan configuration path is {}.", configurationPath);
 		return new WessbasToJmeterConverter(configurationPath);
 	}
 
