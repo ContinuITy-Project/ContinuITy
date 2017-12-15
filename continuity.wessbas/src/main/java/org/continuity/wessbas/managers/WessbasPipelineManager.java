c854c927eb18cd015ceb4acf6f2d86fa6185bb1e
Alper Hidiroglu <Alper Hidiroglu>
2017-12-15 13:48:02 +0100

    Changed WessbasPipelineManager

--- continuity.wessbas/src/main/java/org/continuity/wessbas/managers/WessbasPipelineManager.java
+++ continuity.wessbas/src/main/java/org/continuity/wessbas/managers/WessbasPipelineManager.java
@@ -1,7 +1,12 @@
 package org.continuity.wessbas.managers;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.function.Consumer;
 
+import org.apache.commons.io.FileUtils;
 import org.continuity.wessbas.entities.MonitoringData;
 import org.continuity.wessbas.entities.WessbasDslInstance;
 import org.springframework.util.LinkedMultiValueMap;
@@ -9,6 +14,8 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import m4jdsl.WorkloadModel;
+import net.sf.markov4jmeter.behaviormodelextractor.BehaviorModelExtractor;
+
 
 /**
  * Manages the WESSBAS pipeline from the input data to the output WESSBAS DSL
@@ -46,6 +53,7 @@ public class WessbasPipelineManager {
 
 		convertSessionLogIntoWessbasDSLInstance(sessionLog);
 
+		// Callback gets workloadmodel.xmi output from dslModelGenerator
 		onModelCreatedCallback.accept(WessbasDslInstance.DVDSTORE_PARSED.get());
 	}
 
@@ -55,24 +63,48 @@ public class WessbasPipelineManager {
 	 * @param sessionLog
 	 */
 	private void convertSessionLogIntoWessbasDSLInstance(String sessionLog) {
-		// TODO Auto-generated method stub
+		// Write the session log String into sessions.dat file
+		// Trigger projects
+		// Return workloadmodel.xmi file
+		writeSessionLogIntoFile(sessionLog);
+		BehaviorModelExtractor behav = new BehaviorModelExtractor();
+		behav.createBehaviorModel();
+	}
+
+	private void writeSessionLogIntoFile(String sessionLog) {
+		try {
+			FileOutputStream fout = FileUtils.openOutputStream(new File("examples/inspectit/input/sessions.dat"));
+			PrintStream ps = new PrintStream(fout);
+
+			StringBuffer entry = new StringBuffer();
+			entry.append(sessionLog);
+
+			ps.print(entry.toString());
+
+			ps.close();
+			fout.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
 	/**
+	 * Sends request to Session Logs webservice and gets Session Log
 	 * 
 	 * @param data
 	 * @return
 	 */
 	public String getSessionLog(MonitoringData data) {
-			
+
 		RestTemplate restTemplate = new RestTemplate();
 		MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
 		String urlString = "http://session-logs";
 		map.add("link", data.getLink());
 		String sessionLog = restTemplate.postForObject(urlString, map, String.class);
 		System.out.println(sessionLog.toString());
-		
+
 		return sessionLog;
 	}
 }
